6866935165907d48dba5f30559ca7d48e18304df
CristianG <tapcrt@gmail.com>
2018-07-20 22:24:59 -0500

    Added Post CRUD abilities

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,46 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: %i[show edit update destroy]
+
+  def index; end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+
+    if @post.save
+      redirect_to @post, notice: '¡sí, Cristhian! ¡Tu artículo fue salvado exitosamente!'
+    else
+      render 'new', notice: '¡Oh no, Cristhian! No pude guardar tu publicación.'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @post.update post_params
+      redirect_to @post, notice: '¡Hurra! Tu artículo fue guardado con éxito!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
